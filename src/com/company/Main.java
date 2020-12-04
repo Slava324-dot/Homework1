28bf27fc8bf90635f69cc636783e4ff9aa7f71b6
DESKTOP-LP2J518\Слава <Sinitskogo.8@gmail.com>
2020-12-04 16:57:54 +0300

    Homework1

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 5;
+	while (a > 10) {
+        System.out.println("Мы в цикле While");
+    }
+	do {
+        System.out.println("Мы в цикле Do-While");
+        } while (a > 10);
+	    a = 0;
+	    while (true) {
+	        a++;
+	        if (a == 10) break;
+	        if (a % 2 == 0) continue;
+            System.out.print(a);
+        }
+
+
+
+
+    }
+}
